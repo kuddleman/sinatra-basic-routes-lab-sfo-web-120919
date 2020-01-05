ae4ddf97b38876e952b4df59311d57b8144284df
Donny <EDL1155@gmail.com>
2020-01-04 19:59:13 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Donny"
+  end
+
+  get '/hometown' do
+    "My hometown is Palo Alto, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Orinoco Flow'"
+  end
+
+
 end
